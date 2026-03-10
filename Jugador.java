12ae80c1db27ec5b56fb0b9218a89fe9a5c6e098
carlosalfonso070717-blip <carlosalfonso070717@gmail.com>
2026-03-09 22:23:40 -0600

    Add files via upload

---
+++ Jugador.java
@@ -0,0 +1,71 @@
+class Jugador extends Persona {
+    
+    private double saldo;
+    private int puntosLealtad;
+    private String idJugador;
+
+    public Jugador(String nombre, String apellido, String cedula, int edad, double saldo, String idJugador){
+        super(nombre, apellido, cedula, edad);
+        this.saldo = saldo;
+        this.puntosLealtad = 0;
+        this.idJugador = idJugador;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getPuntosLealtad() {
+        return puntosLealtad;
+    }
+
+    public void setPuntosLealtad(int puntosLealtad) {
+        this.puntosLealtad = puntosLealtad;
+    }
+
+    public String getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(String idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    public void apostar(double monto){
+        if(monto > 0 && monto <= saldo){
+            saldo -= monto;
+            System.out.println("Apuesta realizada. Monto apostado: $ " + monto + ". Saldo restante: " + saldo );
+        }else{
+            System.out.println("La apuesta no se pudo realizar porque el saldo es insuficiente.");
+        }
+    }
+
+    public void recibirPago(double monto){
+        if(monto > 0){
+            saldo += monto;
+            System.out.println("Pago recibido. Su saldo actual es de: $ " + saldo);
+        }
+    }
+
+    @Override
+
+    public String toString(){
+        return "Nombre = " + getNombre() +
+               "\n Apellido = " + getApellido() +
+               "\n Cédula = " + getCedula() +
+               "\n Edad = " + getEdad() +
+               "\n Saldo = " + saldo +
+               "\n Puntos de Lealtad = " + puntosLealtad +
+               "\n ID del Jugador = " + idJugador;
+    }
+
+    @Override
+
+    public String getRol(){
+        return "Jugador";
+    }
+}
---
+++ JugadorVIP.java
@@ -0,0 +1,77 @@
+public class JugadorVIP extends Jugador {
+
+    private String nivelVIP;
+    private double limiteApuestaEspecial;
+    private double porcentajeBonus;
+
+    public JugadorVIP(String nombre, String apellido, String cedula, int edad, 
+                      double saldo, String idJugador, String nivelVIP, 
+                      double limiteApuestaEspecial, double porcentajeBonus) {
+        super(nombre, apellido, cedula, edad, saldo, idJugador);
+        this.nivelVIP = nivelVIP;
+        this.limiteApuestaEspecial = limiteApuestaEspecial;
+        this.porcentajeBonus = porcentajeBonus;
+    }
+
+    public String getNivelVIP() {
+        return nivelVIP;
+    }
+
+    public void setNivelVIP(String nivelVIP) {
+        this.nivelVIP = nivelVIP;
+    }
+
+    public double getLimiteApuestaEspecial() {
+        return limiteApuestaEspecial;
+    }
+
+    public void setLimiteApuestaEspecial(double limite) {
+        this.limiteApuestaEspecial = limite;
+    }
+
+    public double getPorcentajeBonus() {
+        return porcentajeBonus;
+    }
+
+    public void setPorcentajeBonus(double porcentaje) {
+        this.porcentajeBonus = porcentaje;
+    }
+
+    public void aplicarBonus(){
+        double bonus = getSaldo() * (porcentajeBonus/100);
+        setSaldo(getSaldo() + bonus);
+        System.out.println("El bonus de: $ " + bonus + 
+                        "fue aplicado correctamente. Su nuevo saldo es de: $ " + getSaldo());
+    }
+
+    @Override
+    public void apostar(double monto){
+        if(monto > 0 && monto <= limiteApuestaEspecial && monto <= getSaldo()){
+            setSaldo(getSaldo() - monto);
+            System.out.println("VIP: La apuesta realizada fue de: $ " + monto + ". El saldo restante es: $ " + getSaldo());
+        }else{
+            System.out.println("VIP: La apuesta no pudo realizarse porque su saldo es insuficiente o no supera el límite VIP.");
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "\n Nombre = " + getNombre() +
+               "\n Apellido = " + getApellido() +
+               "\n Cédula = " + getCedula() +
+               "\n Edad = " + getEdad() +
+               "\n Saldo = " + getSaldo() +
+               "\n ID del Jugador = " + getIdJugador() +
+               "\n Nivel VIP = " + getNivelVIP() +
+               "\n Límite de Apuesta Especial = " + getLimiteApuestaEspecial() +
+               "\n Porcentaje de Bonus = " + getPorcentajeBonus();
+    }
+
+    @Override
+    public String getRol(){
+        return "Jugador VIP";
+    }
+
+
+    
+}
