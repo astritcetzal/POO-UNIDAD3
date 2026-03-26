427f7723e3a9ebe77e87d67daf72419d7e7f4b38
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-03-26 13:04:22 -0600

    Agregar clase empleadoServicice copiando y cambiando de casinoservice

---
+++ src/casino/servicio/EmpleadoService.java
@@ -0,0 +1,60 @@
+package servicio;
+import java.util.List;
+import persona.Empleado;
+import java.io.IOException;
+import repositorio.PersonaRepository;
+import exceptions.CedulaEmpleadoDuplicadoException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class EmpleadoService {
+    private PersonaRepository repositorio;
+    private List<Empleado> empleados = new ArrayList<>();
+
+    public EmpleadoService (PersonaRepository repositorio)throws IOException {
+        this.repositorio = repositorio;
+        this.empleados= repositorio.cargar();
+    }
+    public void agregarEmpleado(Empleado empleado) throws CedulaEmpleadoDuplicadoException, IOException{
+        if (empleado == null)
+            throw new IllegalArgumentException("El empleado no puede ser nulo");
+        for (Empleado e : empleados) {
+            if (e.getCedula().equals(empleado.getCedula())) {
+                throw new CedulaEmpleadoDuplicadoException(empleado.getCedula());
+            }
+        }
+        empleados.add(empleado);
+        repositorio.guardar(empleados);
+
+    }
+    public Empleado buscarEmpleado(String cedula) {
+        for (Empleado e : empleados)
+            if (e.getCedula().equals(cedula)) {
+                return e;
+            }
+        return null;
+    }
+
+    public List<Empleado> filtrarPorCargo(String cargo) {
+        List<Empleado> resultado = new ArrayList<>();
+
+        for (Empleado e : empleados)
+            if (e.getCargo().equalsIgnoreCase(cargo)) {
+                resultado.add(e);
+            }
+        return resultado;
+    }
+    public void eliminarEmpleado(String cedula) throws IOException {
+        Iterator<Empleado> iterator = empleados.iterator();
+        while (iterator.hasNext()) {
+            Empleado e = iterator.next();
+            if (e.getCedula().equals(cedula)) {
+                iterator.remove();
+                return;
+            }
+        }
+        repositorio.guardar(empleados);
+    }
+
+
+}
